184c6b78f0ec0c33722426d85a64f6e279063baa
Nguyen Van Nguyen <ngvan.nguyen164@gmail.com>
2024-10-30 20:43:44 +0700

    update

---
+++ AK/Data/Models/DaiBanModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AK.Data.Models
+{
+    public class DaiBanModel
+    {
+        public string DiaChiBoDieuKhien { get; set; }
+        public string TenDaiBan { get; set; }
+        public int TinhTrangSuDung { get; set; }
+        public int TinhTrangKetNoi { get; set; }
+        public int ThoiGianDelay { get; set; }
+        public decimal DungLuongPin { get; set; }
+    }
+}
--- AK/Program.cs
+++ AK/Program.cs
@@ -1,13 +1,20 @@
 using AK.Data;
+using ElectronNET.API;
+using ElectronNET.API.Entities;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.WebHost.UseElectron(args);
+builder.Services.AddElectron();
 
 var app = builder.Build();
 
@@ -25,4 +32,20 @@ app.UseRouting();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
-app.Run();
+await app.StartAsync();
+
+// Open the Electron-Window here
+await Electron.WindowManager.CreateWindowAsync(new BrowserWindowOptions()
+{
+    Width = 1600,//1920,
+    Height = 900,//1080,
+    Fullscreenable = true,
+    Fullscreen = true,
+    AcceptFirstMouse = true,
+    Focusable = true,
+    Kiosk = true,
+    Resizable = false,
+    TitleBarStyle = TitleBarStyle.hidden,
+});
+
+app.WaitForShutdown();
